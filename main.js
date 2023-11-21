dfdf759209248899f57175e22c846cd4acf5a887
Rosomak2024 <mbnet@o2.pl>
2023-11-21 12:58:44 +0100

    edit Button

--- main.js
+++ main.js
@@ -1,21 +1,38 @@
+// * Wersja pierwsza - bez array i obiketów działa bez edita.
+
 const addLi = document.querySelector("ul");
 const addButton = document.querySelector(".btn");
 const editButton = document.querySelector(".btn_1");
 const deleteButton = document.querySelector(".btn_2");
-const clearButton = document.querySelector(".btn_3");
 const input = document.querySelector("input");
-
-
+const checkbox = document.getElementById('done');
 
 addButton.addEventListener("click", function inputMaker() {
   const newInput = document.createElement("li");
-  if (input.value === "") {
-    alert("Pozycja musi zawierać przynajmniej 1 znak");
-  } else {
+  const newChecbox = document.createElement("input")
+  // if (input.value === "") {
+  //   alert("Pozycja musi zawierać przynajmniej 1 znak");
+  // } else {
     addLi.appendChild(newInput);
     let text = input.value;
     newInput.innerHTML = text;
+    addLi.appendChild(newChecbox);
+    newChecbox.setAttribute("type","checkbox");
+    newChecbox.setAttribute("id","checbox");
   }
+);
+
+
+editButton.addEventListener("click", function inputEdit() {
+  const liToRemove = document.querySelector("ul");
+  const lastElement = liToRemove.lastElementChild;
+  lastElement.remove();
+  const newInput = document.createElement("li");
+  addLi.appendChild(newInput);
+  newInput.innerHTML = "";
+  let text = input.value;
+  newInput.innerHTML = text;
+  console.log(text);
 });
 
 deleteButton.addEventListener("click", function inputDelete() {
@@ -23,9 +40,3 @@ deleteButton.addEventListener("click", function inputDelete() {
   const lastElement = liToRemove.lastElementChild;
   lastElement.remove();
 });
-
-clearButton.addEventListener("click", function inputClear() {
-  input.value = "";
-});
-
-
