634fa66cc8b1c3cc93d008145e31df72f042f77a
Rosomak2024 <mbnet@o2.pl>
2023-12-01 17:42:08 +0100

    button edit added

--- main.js
+++ main.js
@@ -35,3 +35,29 @@ deleteButton.addEventListener("click", () => {
     ulParrentElement.removeChild(element);
   });
 });
+
+editButton.addEventListener("click", () => {
+  const checkedCheckboxes_toEdit = document.querySelectorAll(
+    'input[type="checkbox"]:checked'
+  );
+  // const li_toEdit = document.querySelectorAll("li");
+
+  checkedCheckboxes_toEdit.forEach((element) => {
+    ulParrentElement.removeChild(element.previousSibling);
+    ulParrentElement.removeChild(element);
+
+    const li = document.createElement("li");
+    // const label = document.createElement("label");
+    const checkbox = document.createElement("input");
+    ulParrentElement.appendChild(li);
+    // ulParrentElement.appendChild(label);
+    ulParrentElement.appendChild(checkbox);
+    li.setAttribute("id", `list_Element_${tasks.length}`);
+    // label.setAttribute("id", `list_Element_Name_${tasks.length}`);
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.setAttribute("id", `checkbox_Element_${tasks.length}`);
+    li.innerHTML = input.value;
+    tasks.push(li);
+    input.value = "";
+  });
+});
