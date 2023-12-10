6d91b6e2d2f03c875a8af28b6e98fa7ed7e0d468
Rosomak2024 <mbnet@o2.pl>
2023-12-10 18:43:00 +0100

    button add hiding

--- main.js
+++ main.js
@@ -1,60 +1,70 @@
-// * Wersja pierwsza - bez array i obiketów działa bez edita.
+// * Wersja pierwsza - bez array i obiketów // First version - without arrays and objects.
 
 const ulParrentElement = document.querySelector("ul");
 const addButton = document.querySelector(".btn");
 const editButton = document.querySelector(".btn_1");
 const deleteButton = document.querySelector(".btn_2");
 const input = document.getElementById("text");
-
+const checkbox = document.getElementById('')
 const tasks = [];
 
-addButton.addEventListener("click", () => {
+addButton.addEventListener("click", (e) => {
   const li = document.createElement("li");
   // const label = document.createElement("label");
   const checkbox = document.createElement("input");
+  if (input.value !== "") {
+    ulParrentElement.appendChild(li);
+    // ulParrentElement.appendChild(label);
+    ulParrentElement.appendChild(checkbox);
+    li.setAttribute("id", `list_Element_${tasks.length}`);
+    // label.setAttribute("id", `list_Element_Name_${tasks.length}`);
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.setAttribute("id", `checkbox_Element_${tasks.length}`);
+    li.innerText = input.value;
+    tasks.push(li);
 
-  ulParrentElement.appendChild(li);
-  // ulParrentElement.appendChild(label);
-  ulParrentElement.appendChild(checkbox);
-  li.setAttribute("id", `list_Element_${tasks.length}`);
-  // label.setAttribute("id", `list_Element_Name_${tasks.length}`);
-  checkbox.setAttribute("type", "checkbox");
-  checkbox.setAttribute("id", `checkbox_Element_${tasks.length}`);
-  li.innerHTML = input.value;
-  tasks.push(li);
-  input.value = "";
-});
-
-deleteButton.addEventListener("click", () => {
-  const checkedCheckboxes = document.querySelectorAll(
-    'input[type="checkbox"]:checked'
-  );
-
-  checkedCheckboxes.forEach((element) => {
-    ulParrentElement.removeChild(element.previousSibling);
-    ulParrentElement.removeChild(element);
-  });
-});
-
-// editButton.addEventListener("click", (e) => {
-//   const checkedCheckboxes_toEdit = document.querySelector(
-//     'input[type="checkbox"]:checked'
-//   );
-
-editButton.addEventListener("click", function (e) {
-  const checkedCheckboxes_toEdit = document.querySelector(
-    'input[type="checkbox"]:checked'
-  );
-  if (e.isTrusted && checkedCheckboxes_toEdit.value) {
-    checkedCheckboxes_toEdit.previousElementSibling.innerHTML = input.value;
     input.value = "";
-
-    console.log("działa");
-    // console.log("info",info );
+  } else {
+    alert("Musisz wpisać tekst");
   }
+});
 
+deleteButton.addEventListener("click", (e) => {
+  const checkedCheckbox = document.querySelectorAll(
+    'input[type="checkbox"]:checked'
+    );
+    
+    checkedCheckbox.forEach((element) => {
+      ulParrentElement.removeChild(element.previousSibling);
+      ulParrentElement.removeChild(element);
+    });
+  });
+  
+  editButton.addEventListener("click", function (e) {
+    const checkedCheckbox_toEdit = document.querySelector(
+      'input[type="checkbox"]:checked'
+      );
+      
+      if (input.value == "") {
+        alert("Pole edycji jest puste");
+      } else if (e.isTrusted && checkedCheckbox_toEdit.value) {
+        checkedCheckbox_toEdit.previousElementSibling.innerText = input.value;
+    input.value = "";
+  }
+  // console.log("działa");
+  // console.log("info",info );
+  
   // console.log("e",e );
   // console.log("checkedCheckboxes_toEdit",checkedCheckboxes_toEdit );
   // const textContent = checkedCheckboxes_toEdit.previousElementSibling;
   // e.target.textContent = input.value;
+});
+
+
+
+input.addEventListener('click',() => {
+  if(input.type == 'checkbox' && input.checked == 1) {
+    addButton.disabled = false;
+  console.log('jest' );
+  }
 });
