0b29d5d1f3ec6b3fcff19942d6bc9d0a611b74f2
Rosomak2024 <mbnet@o2.pl>
2024-01-28 12:10:03 +0100

    document listener added

--- main.js
+++ main.js
@@ -3,12 +3,70 @@ const addButton = document.querySelector(".btn");
 const editButton = document.querySelector(".btn_1");
 const deleteButton = document.querySelector(".btn_2");
 const input = document.getElementById("text");
+
 const tasks = [];
 
-addButton.addEventListener("click", () => {
-  const li = document.createElement("li");
-  const checkbox = document.createElement("input");
-  if (input.value !== "") {
+// addButton.addEventListener("click", () => {
+  //   const li = document.createElement("li");
+  //   const checkbox = document.createElement("input");
+  //   if (input.value !== "") {
+    //     olParrentElement.appendChild(li);
+    //     olParrentElement.appendChild(checkbox);
+//     li.setAttribute("id", `list_Element_${tasks.length}`);
+//     checkbox.setAttribute("type", "checkbox");
+//     checkbox.setAttribute("id", `checkbox_Element_${tasks.length}`);
+//     li.innerText = input.value;
+//     tasks.push(li);
+//     input.value = "";
+//   } else {
+  //     alert("Musisz wpisać tekst");
+  //   }
+  // });
+  
+  // deleteButton.addEventListener("click", () => {
+    //   const checkedCheckbox = document.querySelectorAll(
+      //     'input[type="checkbox"]:checked');
+      
+      //   checkedCheckbox.forEach((element) => {
+        //     olParrentElement.removeChild(element.previousSibling);
+        //     olParrentElement.removeChild(element);
+        //   });
+        // });
+        
+        // editButton.addEventListener("click", () => {
+          //   const checkedCheckbox_toEdit = document.querySelector(
+            //     'input[type="checkbox"]:checked');
+            
+            //   if (input.value == "") {
+              //     alert("Pole edycji jest puste");
+              //   } else {
+                //     checkedCheckbox_toEdit.previousElementSibling.innerText = input.value;
+//     input.value = "";
+//   }
+// });
+
+// document.addEventListener("click", function (e) {
+  //   const target = e.target.closest("input[type='checkbox']");
+
+  //   if (target) {
+    //     addButton.disabled = true;
+    //     editButton.disabled = false;
+    //     deleteButton.disabled = false;
+    //   } else if (input.value !== "") {
+      //     addButton.disabled = false;
+      //     editButton.disabled = true;
+      //     deleteButton.disabled = true;
+      //   }
+      // });
+      
+      
+document.addEventListener('click',function(event){
+  
+  console.log("event",event);
+  if(input.value !== "" && event.target === addButton ){
+
+    const li = document.createElement("li");
+    const checkbox = document.createElement("input");
     olParrentElement.appendChild(li);
     olParrentElement.appendChild(checkbox);
     li.setAttribute("id", `list_Element_${tasks.length}`);
@@ -17,43 +75,35 @@ addButton.addEventListener("click", () => {
     li.innerText = input.value;
     tasks.push(li);
     input.value = "";
-  } else {
-    alert("Musisz wpisać tekst");
+
+  } else if (event.target === deleteButton) {
+
+        const checkedCheckbox = document.querySelectorAll(
+       'input[type="checkbox"]:checked');
+        checkedCheckbox.forEach((element) => {
+          olParrentElement.removeChild(element.previousSibling);
+          olParrentElement.removeChild(element);
+        });
+  } else if (event.target === editButton) {
+
+    const checkedCheckbox_toEdit = document.querySelector(
+          'input[type="checkbox"]:checked');
+      
+          checkedCheckbox_toEdit.previousElementSibling.innerText = input.value;
+          input.value = "";
   }
-});
 
-deleteButton.addEventListener("click", () => {
-  const checkedCheckbox = document.querySelectorAll(
-    'input[type="checkbox"]:checked');
 
-  checkedCheckbox.forEach((element) => {
-    olParrentElement.removeChild(element.previousSibling);
-    olParrentElement.removeChild(element);
-  });
-});
+    // if (target) {
+    //   addButton.disabled = true;
+    //   editButton.disabled = false;
+    //   deleteButton.disabled = false;
+    // } else if (input.value !== "") {
+    //     addButton.disabled = false;
+    //   editButton.disabled = true;
+    //   deleteButton.disabled = true;
+    // }
 
-editButton.addEventListener("click", () => {
-  const checkedCheckbox_toEdit = document.querySelector(
-    'input[type="checkbox"]:checked');
 
-  if (input.value == "") {
-    alert("Pole edycji jest puste");
-  } else {
-    checkedCheckbox_toEdit.previousElementSibling.innerText = input.value;
-    input.value = "";
-  }
-});
 
-document.addEventListener("click", function (e) {
-  const target = e.target.closest("input[type='checkbox']");
-
-  if (target) {
-    addButton.disabled = true;
-    editButton.disabled = false;
-    deleteButton.disabled = false;
-  } else if (input.value !== "") {
-    addButton.disabled = false;
-    editButton.disabled = true;
-    deleteButton.disabled = true;
-  }
-});
+})
