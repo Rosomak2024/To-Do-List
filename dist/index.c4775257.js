e412249100137ffe868f9c17b239c3ee069cd5b0
Rosomak2024 <mbnet@o2.pl>
2024-02-05 14:20:39 +0100

    Added textInput and checkbox handling

--- dist/index.c4775257.js
+++ dist/index.c4775257.js
@@ -1,98 +1,65 @@
-// const olParrentElement = document.querySelector("ol");
-// const addButton = document.querySelector(".btn");
-// const editButton = document.querySelector(".btn_1");
-// const deleteButton = document.querySelector(".btn_2");
-// const input = document.getElementById("text");
-// const tasks = [];
-// addButton.disabled = false;
-// editButton.disabled = true;
-// deleteButton.disabled = true;
-// document.addEventListener("click", function (event) {
-//   console.log("event", event);
-//   handleButtons(event);
-//   handleDisplayButtons(event);
-// });
-// function handleButtons(event) {
-//   if (input.value !== "" && event.target === addButton) {
-//     const li = document.createElement("li");
-//     const checkbox = document.createElement("input");
-//     olParrentElement.appendChild(li);
-//     olParrentElement.appendChild(checkbox);
-//     li.setAttribute("id", `list_Element_${tasks.length}`);
-//     checkbox.setAttribute("type", "checkbox");
-//     checkbox.setAttribute("id", `checkbox_Element_${tasks.length}`);
-//     li.innerText = input.value;
-//     tasks.push(li);
-//     input.value = "";
-//   } else if (event.target === deleteButton) {
-//     const checkedCheckbox = document.querySelectorAll(
-//       'input[type="checkbox"]:checked'
-//     );
-//     checkedCheckbox.forEach((element) => {
-//       olParrentElement.removeChild(element.previousSibling);
-//       olParrentElement.removeChild(element);
-//     });
-//   } else if (event.target === editButton) {
-//     const checkedCheckbox_toEdit = document.querySelector(
-//       'input[type="checkbox"]:checked'
-//     );
-//     checkedCheckbox_toEdit.previousElementSibling.innerText = input.value;
-//     input.value = "";
-//   }
-// }
-// function handleDisplayButtons(event) {
-//   const newCheckbox = event.target.closest("input[type='checkbox']");
-//   if (newCheckbox) {
-//     addButton.disabled = true;
-//     editButton.disabled = false;
-//     deleteButton.disabled = false;
-//   } else if (input.value !== "") {
-//     addButton.disabled = false;
-//     editButton.disabled = true;
-//     deleteButton.disabled = true;
-//   }
-// }
 const olParrentElement = document.querySelector("ol");
 const addButton = document.querySelector(".btn");
 const editButton = document.querySelector(".btn_1");
 const deleteButton = document.querySelector(".btn_2");
-const textInput = document.getElementById("text");
+const textInput = document.getElementById("text-input");
 const tasks = [];
+document.querySelectorAll("input[type='checkbox']");
 // resetButtonState();
-resetTextInputState();
+// resetTextInputState();
 document.addEventListener("keyup", function(event) {
     const isEventInTextInput = event.target === textInput;
-    if (isEventInTextInput) handleTextInput(event);
+    if (isEventInTextInput) handleTextInput(event.target.value); // event 
 });
 document.addEventListener("click", function(event) {
     const isEventOnButton = event.target === addButton || event.target === deleteButton || event.target === editButton;
+    const isEventOnCheckbox = event.target.type === "checkbox";
     if (isEventOnButton) handleButton(event);
+    else if (isEventOnCheckbox) handleCheckbox(event);
 });
 function handleButton(event) {
-    console.log("3 handleButton", event);
     if (event.target == addButton) addTaskToList();
     else if (event.target == deleteButton) deleteTaskFromList();
     else if (event.target == editButton) editTask();
-// resetTextInputState();}
-// jesli edit kliniety
-// jesli delete kliniety
 }
-function handleTextInput(event) {
-    console.log("handleTextInput", event.target.value);
-// jeśli text input pusty to edit i delete disabled
+function handleCheckbox(event) {
+    const checkboxInputs = document.querySelectorAll("input[type='checkbox']");
+    const isAllUnchecked = [
+        ...checkboxInputs
+    ].every((checkboxInput)=>checkboxInput.checked === false);
+    console.log("isAllUnchecked", isAllUnchecked);
+    if (isAllUnchecked) resetButtonState();
+    else resetButtonStateWhenEdit();
+}
+function handleTextInput(value) {
+    // jeśli text input pusty to edit i delete disabled
+    console.log("handletextinut", value);
+    if (textInput !== "") {
+        resetButtonState();
+        console.log("Input not empty");
+    } else if (textInput === "") {
+        // alert('Please write down some text!')
+        resetButtonStateWhenEdit();
+        console.log("Input empty");
+    }
 // jesli text input nie pusty to text
 // ZDARZENIE ADD BUTTON CLICK
 // 1 addTaskToList
 // 2 resetButtonState
 // 3 resetTextInputState
 }
-// function resetButtonState() {
-//   addButton.disabled = false;
-//   editButton.disabled = true;
-//   deleteButton.disabled = true;
-// }
+function resetButtonState() {
+    addButton.disabled = false;
+    editButton.disabled = true;
+    deleteButton.disabled = true;
+}
+function resetButtonStateWhenEdit() {
+    addButton.disabled = true;
+    editButton.disabled = false;
+    deleteButton.disabled = false;
+}
 function resetTextInputState() {
-    textInput;
+    textInput.value = "";
 }
 function addTaskToList() {
     const li = document.createElement("li");
@@ -104,6 +71,7 @@ function addTaskToList() {
     checkbox.setAttribute("id", `checkbox_Element_${tasks.length}`);
     li.innerText = textInput.value;
     tasks.push(li);
+    resetTextInputState();
 }
 function deleteTaskFromList() {
     const checkedCheckbox = document.querySelectorAll("input[type='checkbox']:checked");
@@ -117,6 +85,7 @@ function editTask() {
     const checkedCheckbox_toEdit = document.querySelector('input[type="checkbox"]:checked');
     // jak to moge zapisać oddzielnie
     checkedCheckbox_toEdit.previousElementSibling.innerText = textInput.value;
+    resetTextInputState();
 }
 
 //# sourceMappingURL=index.c4775257.js.map
